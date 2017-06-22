8abd543c765664c8c86ede9cf0015669ce19595b
Sean Fitzgerald <Sean.R.Fitzgerald@outlook.com>
2017-06-21 20:10:17 -0500

    Created the erb template, created methods to pull legislators based on zipcode, and began creating method to push inputs to erb template.

---
+++ lib/event_manager.rb
@@ -0,0 +1,28 @@
+require "csv"
+require "sunlight/congress"
+require "erb"
+
+Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
+
+def clean_zipcode(zipcode)
+  	zipcode.to_s.rjust(5,"0")[0..4]
+end
+
+def legislators_by_zipcode(zipcode)
+	legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
+end
+
+template_letter = File.read "form_letter.erb"
+erb_template = ERB.new template_letter
+
+contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
+
+contents.each do |row|
+	first_name = row[:first_name]
+	zipcode = clean_zipcode(row[:zipcode])
+	legislators = legislators_by_zipcode(zipcode)
+	
+	form_letter = erb_template.result(binding)
+
+	puts form_letter
+end
